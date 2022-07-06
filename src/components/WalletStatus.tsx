f0071f37f39ffb3ad275c10d5043002875827c7e
Mattia Pomelli <mattiapomelli@gmail.com>
2022-07-06 09:27:07 +0200

    Show user balance and address verification

--- src/components/WalletStatus.tsx
+++ src/components/WalletStatus.tsx
@@ -7,13 +7,11 @@ import Address from '@components/Address'
 import { useWeb3Context } from '@contexts/Web3Provider'
 import { useUser, UserStatus } from '@contexts/AuthProvider'
 import Button from '@components/Button'
+import { ethers } from 'ethers'
 
 const WalletStatus = () => {
-  const { account, connect, error } = useWeb3Context()
-  const {
-    status,
-    // verifyAddress
-  } = useUser()
+  const { account, connect, error, balance } = useWeb3Context()
+  const { status, verifyAddress } = useUser()
 
   const iconRef = useRef<HTMLSpanElement>(null)
   const icon = useMemo(
@@ -46,11 +44,12 @@ const WalletStatus = () => {
   if (status === UserStatus.Connected && account) {
     return (
       <div className="flex items-center gap-2 bg-gray-200 py-1.5 px-2 rounded-full">
+        <span>{ethers.utils.formatEther(balance)}</span>
         <span>
           <Address address={account} className="font-semibold" />
-          {/* <button className="ml-2 text-sm font-medium" onClick={verifyAddress}>
+          <button className="ml-2 text-sm font-medium" onClick={verifyAddress}>
             Verify address
-          </button> */}
+          </button>
         </span>
         <span ref={iconRef} className="inline-flex" />
       </div>
@@ -62,6 +61,7 @@ const WalletStatus = () => {
     return (
       <Link href="/profile">
         <a className="flex items-center gap-2 bg-gray-200 py-1.5 px-2 rounded-full">
+          <span>{ethers.utils.formatEther(balance)}</span>
           <span>
             <Address address={account} className="font-semibold" />
           </span>
