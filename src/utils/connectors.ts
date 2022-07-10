f48455909373f85e3a2a953a87d1617c0f13ddbe
Mattia Pomelli <mattiapomelli@gmail.com>
2022-07-10 19:46:44 +0200

    Add constants for supported chains

--- src/utils/connectors.ts
+++ src/utils/connectors.ts
@@ -1,5 +1,7 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
 
+import { ALL_SUPPORTED_CHAIN_IDS } from '@constants/chains'
+
 export const injected = new InjectedConnector({
-  supportedChainIds: [31337, 80001], // TODO: add production chain id
+  supportedChainIds: ALL_SUPPORTED_CHAIN_IDS,
 })
