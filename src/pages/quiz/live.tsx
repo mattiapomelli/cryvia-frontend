fab97c2edcb29f3c60148840ee249479db23a6df
Mattia Pomelli <mattiapomelli@gmail.com>
2022-07-13 16:16:16 +0200

    Check if user is subscribed to live quiz

--- src/pages/quiz/live.tsx
+++ src/pages/quiz/live.tsx
@@ -4,11 +4,19 @@ import { useQuery } from 'react-query'
 import QuizProvider from '@components/Quiz/QuizProvider'
 import { useApiClient } from '@contexts/AuthProvider'
 import QuizContainer from '@components/Quiz/QuizContainer'
+import { useQuizContract } from '@hooks/useContract'
+import { useEffect } from 'react'
+import { useWeb3Context } from '@contexts/Web3Provider'
+import { useRouter } from 'next/router'
 
 let done = false
 
-const QuizPage: NextPage = () => {
+const LiveQuizPage: NextPage = () => {
+  const { account } = useWeb3Context()
   const apiClient = useApiClient()
+  const router = useRouter()
+  const quizContract = useQuizContract(true)
+
   const { data: quiz } = useQuery(
     'nextQuiz',
     () => apiClient.quizzes.next().then((data) => data.data),
@@ -18,6 +26,18 @@ const QuizPage: NextPage = () => {
       refetchOnWindowFocus: false,
     },
   )
+
+  useEffect(() => {
+    if (!quiz?.id || !quizContract || !account) return
+
+    // If quiz is not subscribed to the current qui, redirect to quiz page
+    const getIfSubscribed = async () => {
+      const isSubscribed = await quizContract.isSubscribed(quiz.id, account)
+      if (!isSubscribed) router.push(`/quiz/${quiz.id}`)
+    }
+
+    getIfSubscribed()
+  }, [quiz?.id, account, quizContract, router])
 
   if (!quiz) return null
 
@@ -35,4 +55,4 @@ const QuizPage: NextPage = () => {
   )
 }
 
-export default QuizPage
+export default LiveQuizPage
