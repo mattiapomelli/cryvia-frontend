47f4d1368986576b8d15a9bbe05d4dacbce7e8d4
Mattia Pomelli <mattiapomelli@gmail.com>
2022-07-19 11:29:53 +0200

    Add minting page

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode } from 'react'
 
 import Navbar from '@components/Layout/Navbar'
 import Footer from '@components/Layout/Footer'
+import Container from '@components/Layout/Container'
+import Link from 'next/link'
 
 interface DefaultLayoutProps {
   children: ReactNode
@@ -11,7 +13,19 @@ const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow pb-20">{children}</main>
+      <main className="flex-grow pb-20">
+        <Container>
+          <div className="mt-4 bg-blue-100 text-primary p-4 rounded-xl text-center">
+            Go{' '}
+            <Link href="/mint">
+              <a className="underline font-bold">here</a>
+            </Link>{' '}
+            to mint some free tokens to test the platform
+          </div>
+        </Container>
+
+        {children}
+      </main>
       <Footer />
     </div>
   )
