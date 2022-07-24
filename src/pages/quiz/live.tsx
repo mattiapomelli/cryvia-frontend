868214957f65a84b896487be6aae4f29c7cc12e0
Mattia Pomelli <mattiapomelli@gmail.com>
2022-07-24 13:55:56 +0200

    Refactor redirect if user not subscribed to quiz

--- src/pages/quiz/live.tsx
+++ src/pages/quiz/live.tsx
@@ -1,24 +1,34 @@
 import { useQuery } from 'react-query'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 import QuizProvider, {
   QuizPlayingStatus,
   useQuiz,
 } from '@components/Quiz/Game/QuizProvider'
 import { useApiClient } from '@contexts/AuthProvider'
-import { useQuizContract } from '@hooks/useContract'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import WaitingRoom from '@components/Quiz/Game/WaitingRoom'
 import Quiz from '@components/Quiz/Game/Quiz'
 import FinalRoom from '@components/Quiz/Game/FinalRoom'
 import { PageWithLayout } from 'types'
 import Container from '@components/Layout/Container'
-import { useAccount } from 'wagmi'
+import useSubscriptionStatus, {
+  SubscriptionStatus,
+} from '@hooks/useSubscriptionStatus'
 
 // let done = false
 
 const LiveQuizPageInner = () => {
-  const [{ status }] = useQuiz()
+  const [{ status, quiz }] = useQuiz()
+  const router = useRouter()
+  const { loading, status: subscriptionStatus } = useSubscriptionStatus(quiz)
+
+  useEffect(() => {
+    // If user hasn't subscribed to quiz, redirect to quiz page
+    if (!loading && subscriptionStatus !== SubscriptionStatus.Subscribed) {
+      router.push(`/quiz/${quiz.id}`)
+    }
+  }, [loading, subscriptionStatus, router, quiz.id])
 
   if (status === QuizPlayingStatus.Waiting) {
     return <WaitingRoom />
@@ -32,10 +42,8 @@ const LiveQuizPageInner = () => {
 }
 
 const LiveQuizPage: PageWithLayout = () => {
-  const { address } = useAccount()
   const apiClient = useApiClient()
   const router = useRouter()
-  const quizContract = useQuizContract(true)
 
   const { data: quiz, isLoading } = useQuery(
     'nextQuiz',
@@ -51,20 +59,8 @@ const LiveQuizPage: PageWithLayout = () => {
     // If there is no next quiz at the moment, redirect to homepage
     if (!isLoading && !quiz) {
       router.push(`/`)
-      return
     }
-
-    if (!quiz?.id || !quizContract || !address) return
-
-    // TODO: uncomment this for production
-    // If quiz is not subscribed to the current qui, redirect to quiz page
-    // const getIfSubscribed = async () => {
-    //   const isSubscribed = await quizContract.isSubscribed(quiz.id, account)
-    //   if (!isSubscribed) router.push(`/quiz/${quiz.id}`)
-    // }
-
-    // getIfSubscribed()
-  }, [quiz, address, quizContract, router, isLoading])
+  }, [quiz, router, isLoading])
 
   if (!quiz) return null
 
