32521d56b3a1c665916a19c4efefb4526ba3c016
Mattia Pomelli <mattiapomelli@gmail.com>
2022-08-01 09:33:39 +0200

    Add query param to revalidate single quiz page

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -13,8 +13,12 @@ export default async function handler(
     try {
       // this should be the actual path not a rewritten path
       // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-      await res.revalidate('/')
-      return res.json({ revalidated: true })
+
+      const quizId = req.query.quizId?.toString()
+      const path = quizId ? `/quizzes/${quizId}` : '/'
+
+      await res.revalidate(path)
+      return res.json({ path, revalidated: true })
     } catch (err) {
       // If there was an error, Next.js will continue
       // to show the last successfully generated page
