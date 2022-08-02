8fa19d3fe3539aaca436f7a3c89a14efe22df976
Mattia Pomelli <mattiapomelli@gmail.com>
2022-08-02 07:44:04 +0200

    Fade in wallet status component after user loaded

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,16 +1,31 @@
+import { Transition } from '@headlessui/react'
+
 import Container from '@/components/Layout/Container'
 import Logo from '@/components/Logo'
 import WalletStatus from '@/components/WalletStatus'
 import { UserStatus, useUser } from '@/contexts/AuthProvider'
+import useTransitionControl from '@/hooks/useTransitionControl'
 
 const Navbar = () => {
   const { status } = useUser()
+  const [show] = useTransitionControl(status === UserStatus.Loading)
 
   return (
-    <header className="py-4">
+    <header className="flex items-center h-20">
       <Container className="flex justify-between items-center w-full">
         <Logo />
-        {status !== UserStatus.Loading && <WalletStatus />}
+        <Transition
+          show={show}
+          enter="transition-opacity duration-200"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          className="hidden md:block"
+        >
+          <WalletStatus />
+        </Transition>
       </Container>
     </header>
   )
