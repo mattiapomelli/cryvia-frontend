d2b81cfbadc8d63ccc5f4adf6ede91aafe7fe289
Mattia Pomelli <mattiapomelli@gmail.com>
2022-08-06 10:47:28 +0200

    Add message to final room about setting winners

--- src/components/Quiz/Game/FinalRoom.tsx
+++ src/components/Quiz/Game/FinalRoom.tsx
@@ -5,7 +5,7 @@ const FinalRoom = () => {
 
   return (
     <>
-      <div className="bg-tertiary flex flex-col gap-2 p-4 rounded-default items-center mt-20">
+      <div className="bg-tertiary flex flex-col gap-2 p-4 rounded-default items-center mt-20 max-w-md mx-auto">
         <p className="text-text-secondary">You have completed</p>
         <h1 className="text-3xl font-bold text-center">{quiz.title}</h1>
         <p className="text-3xl">🎉</p>
@@ -13,7 +13,17 @@ const FinalRoom = () => {
           <span className="font-bold">{playersCount} </span>
           people have completed the quiz.
         </p>
-        <p>Waiting for other users to finish...</p>
+        <div className="text-center flex flex-col gap-2 mt-2 mb-2">
+          <p className="mb-4">
+            Waiting for other users to finish and then setting winners on smart
+            contract...
+          </p>
+          <p className="text-sm  bg-[#fff2ab] p-3 rounded-default">
+            This may take a while, sometimes Mumbai Testnet transactions take
+            several minutes to be confirmed... You can leave this page and come
+            back in a while to discover if you won!
+          </p>
+        </div>
       </div>
     </>
   )
